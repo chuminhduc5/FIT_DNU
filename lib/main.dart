c6f905415f7ec9f65edc6887242c7901a972ef07
Haidang <nguyendang2467@gmail.com>
2024-11-01 22:46:56 +0700

    Update ProfileScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fit_dnu/core/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'feature/authentication/presentation/screens/sign_in_screen.dart';
+import 'feature/profile/presentation/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInScreen(),
+      home: ProfileScreen(),
       theme: AppTheme.appTheme,
     );
   }
